cf2b3bdfb2a4886a5205f87a63a79fad8fcf8056
victor030220 <rmvictor20@gmail.com>
2025-04-01 00:27:24 -0300

    Se subio el contenido del curso, todo de 0

---
+++ Desafios/desafio1.js
@@ -0,0 +1,79 @@
+// Datos de entrada (para pruebas)
+const loanAmount = 10000;
+const annualInterestRate = 12;  // 12%
+const termInMonths = 24;
+const clientType = "returning";
+
+const UPPERLOANLIMIT = 50000
+const LOWERLOANLIMIT = 1000;
+const UPPERPORCENTAGELIMIT = 20
+const LOWERPORCENTAGELIMIT = 5
+const UPPERTIMELIMIT = 60
+const LOWERTIMELIMIT = 6
+const TYPESOFCLIENTS = new Map();
+
+TYPESOFCLIENTS.set("new",0)//expresados en %
+TYPESOFCLIENTS.set("returning", 2)
+TYPESOFCLIENTS.set("premium", 5)
+
+function calculateMonthlyPayment(amount, rate, term) //El enunciado expresa term en meses
+{
+    rate = (rate/100) / 12 //Divido por 100 para poder multiplicarlo, calculo la tasa mensual ya que viene expresada en forma anual, divido por 12
+    amount = amount + ( amount * rate * term ) //obtengo el total que debo pagar en ese lapso de tiempo
+    return amount / term
+}
+
+function calculateTotalPayment(monthlyPayment, term)
+{
+    return monthlyPayment * term
+}
+
+function calculateTotalInterest(totalPayment, loanAmount)
+{
+    return totalPayment - loanAmount
+}
+
+function applyDiscount(monthlyPayment, clientType)
+{
+    let discountPorcentage
+
+    discountPorcentage = TYPESOFCLIENTS.get(clientType) / 100 //divido por 100 para poder multiplicarlo
+    return monthlyPayment * (1 - discountPorcentage)
+
+}
+
+function isWithinTheRange( amount,lowerLimit, upperLimit )
+{
+    return lowerLimit >= amount && amount <= upperLimit
+}
+
+// Función principal
+function calculateLoan(amount, rate, term, type) {
+    
+    let monthlyPayment, totalPayment, totalInterest, discountedTotal
+
+    if( ! isWithinTheRange(amount,LOWERLOANLIMIT, UPPERLOANLIMIT) )
+        return -1 //damos un codigo de error en los parametros ingresados
+
+    if( ! isWithinTheRange(rate, LOWERPORCENTAGELIMIT, UPPERPORCENTAGELIMIT) )
+        return -1
+
+    if( ! isWithinTheRange(term, LOWERTIMELIMIT, UPPERTIMELIMIT) )
+        return -1
+
+    if( ! TYPESOFCLIENTS.has(type) )
+        return -1
+
+    monthlyPayment = calculateMonthlyPayment(amount, rate, term)
+    totalPayment = calculateTotalPayment(monthlyPayment, term)
+    totalInterest = calculateTotalInterest (totalInterest, amount)
+    discountedTotal = applyDiscount(monthlyPayment, type)
+    
+}
+
+// Ejecuta el programa y muestra resultados
+//const result = calculateLoan(loanAmount, annualInterestRate, termInMonths, clientType);
+//console.log("=== DETALLES DEL PRÉSTAMO ===");
+// Muestra los resultados aquí
+
+console.log(calculateMonthlyPayment(1000, 20, 3))
